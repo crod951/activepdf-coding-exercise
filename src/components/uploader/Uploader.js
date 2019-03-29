f33f85f83ea81b4737f8a17bf258d859f0bf395d
Chris Rodrigues <crod951@gmail.com>
2019-03-28 18:21:25 -0700

    Uploader is now hidden once upload is complete.

--- src/components/uploader/Uploader.js
+++ src/components/uploader/Uploader.js
@@ -22,6 +22,7 @@ class Uploader extends Component {
             fetch("http://localhost:3000/IMG.pdf")
             .then(res => res.blob())
             .then(blob => {
+              this.props.onChange();
               window.initReaderPlus(blob);
             });
             console.log('onprocessfile', fileItem.filename, fileItem.source);      
